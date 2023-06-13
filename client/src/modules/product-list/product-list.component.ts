1837d9bb4d49d551d0c495b077a699e2b96d3913
joaoleaogf <joaoleao.gf@gmail.com>
2023-06-13 01:51:13 -0300

    fix: product card style

--- client/src/modules/product-list/product-list.component.ts
+++ client/src/modules/product-list/product-list.component.ts
@@ -8,34 +8,29 @@ import { Component, Input } from '@angular/core';
 export class ProductListComponent {
   @Input() products: any[] = [
     {
-      name: 'Produto 1',
-      description: 'Descrição do Produto 1',
-      price: 19.99,
-      imageUrl: 'path/to/image1.jpg'
+      description: 'Macbook Apple Macbook Air A2337 Apple M1 13,3" 8GB SSD 256 GB Mac OS Leitor Biométrico',
+      price: 5722.03,
+      src: 'https://i.zst.com.br/thumbs/45/28/34/1369411026.jpg'
     },
     {
-      name: 'Produto 2',
-      description: 'Descrição do Produto 2',
-      price: 29.99,
-      imageUrl: 'path/to/image2.jpg'
+      description: 'Smartphone Xiaomi Redmi Note 12 6GB RAM 128GB Câmera Tripla',
+      price: 1175,
+      src: 'https://i.zst.com.br/thumbs/45/17/18/-985316511.jpg'
     },
     {
-      name: 'Produto 3',
-      description: 'Descrição do Produto 3',
-      price: 39.99,
-      imageUrl: 'path/to/image3.jpg'
+      description: 'Geladeira Brastemp BRE57AK Frost Free Inverse 443 Litros cor Inox',
+      price: 4011.15,
+      src: 'https://i.zst.com.br/thumbs/45/24/b/194270709.jpg'
     },
     {
-      name: 'Produto 4',
-      description: 'Descrição do Produto 4',
-      price: 49.99,
-      imageUrl: 'path/to/image4.jpg'
+      description: 'Tablet Samsung Galaxy Tab S6 Lite SM-P615N 64GB 4G 10,4" Android',
+      price: 1994.05,
+      src: 'https://i.zst.com.br/thumbs/45/23/35/1110566199.jpg'
     },
     {
-      name: 'Produto 4',
-      description: 'Descrição do Produto 4',
-      price: 49.99,
-      imageUrl: 'path/to/image4.jpg'
+      description: 'Smartphone Xiaomi Pocophone Poco X5 5G 128GB Câmera Tripla',
+      price: 1496.52,
+      src: 'https://i.zst.com.br/thumbs/45/1/34/-975675791.jpg'
     }
   ];
 }
