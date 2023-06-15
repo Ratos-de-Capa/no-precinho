75d464fcf96aa5f078b033c790185299d573d19a
Joao Vitor <joaovitorb3105@gmail.com>
2023-06-15 16:48:57 -0300

    Padronizando codigo com o uso de componentes na Pagina de Compra

---
+++ client/src/modules/purchase-carousel/purchase-carousel.module.ts
@@ -0,0 +1,18 @@
+import { PurchaseCarouselComponent } from './purchase-carousel.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [
+    PurchaseCarouselComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PurchaseCarouselComponent
+  ]
+})
+export class PurchaseCarouselModule { }
