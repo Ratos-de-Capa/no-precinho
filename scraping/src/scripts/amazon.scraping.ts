fa79506e9e4e5576bd2cd66e41ee65e5c81900a1
leandrobalta <leandrobraga.job@gmail.com>
2023-07-04 23:47:22 -0300

    bug solved: scraping blocked, now have one browser for each page evaluate

--- scraping/src/scripts/amazon.scraping.ts
+++ scraping/src/scripts/amazon.scraping.ts
@@ -12,65 +12,25 @@ export class AmazonScraping extends Scraping {
   async search(search: Category): Promise<Product[]> {
     const item = search.name;
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // const url = `https://www.amazon.com.br/s?k=${item}`;
-    // await page.goto(url);
+    const basicProducts = await this.getBasicProducts(item);
 
-    page.on('console', (msg) => {
-      for (let i = 0; i < msg.args().length; ++i)
-        console.log(`${i}: ${msg.args()[i]}`);
-    });
+    const products: Product[] = [];
 
-    // const basicProducts: BasicProduct[] = await page.evaluate(() => {
-    //   const productElements = Array.from(
-    //     document.querySelectorAll(".a-section.a-spacing-base")
-    //   );
-
-    //   const productsAux: BasicProduct[] = [];
-
-    //   for (const element of productElements) {
-    //     const nameElement = element.querySelector<HTMLSpanElement>(".a-size-base-plus.a-color-base.a-text-normal");
-    //     const priceWholeElement = element.querySelector<HTMLSpanElement>(".a-price-whole");
-    //     const priceFractionElement = element.querySelector<HTMLSpanElement>(".a-price-fraction");
-    //     const imageElement = element.querySelector<HTMLImageElement>(".s-image");
-    //     const linkElement = element.querySelector<HTMLAnchorElement>(".a-link-normal.a-text-normal");
-
-    //     const name = nameElement ? nameElement.innerText.trim() : "";
-    //     const priceWhole = priceWholeElement ? priceWholeElement.innerText.trim() : "";
-    //     const priceFraction = priceFractionElement ? priceFractionElement.innerText.trim() : "";
-    //     const image = imageElement ? imageElement.src : "";
-    //     const link = linkElement ? linkElement.href : "";
-
-    //     // //const price = priceWhole && priceFraction ? `${priceWhole}.${priceFraction}` : '';
-    //     const price = priceWhole && priceFraction ? parseFloat(`${priceWhole.replace(/\./g, '')}.${priceFraction}`.replace(/[\n,]/g, "")) : null;
-
-    //     const pushProduct = {
-    //       name,
-    //       price,
-    //       imageSource: image,
-    //       link,
-    //     } as BasicProduct;
-
-    //     if (pushProduct.name === '' || pushProduct.price === null || pushProduct.imageSource === '' || pushProduct.link === '') {
-    //       continue;
-    //     }
-
-    //     productsAux.push(pushProduct) 
-    //   }
-
-    //   return productsAux;
-    // });
-    const basicProducts = await this.getBasicProducts(item, page);
-
-    const products: Product[] = await this.getProductsDetails(basicProducts, page);
+    for (let i = 0; i < basicProducts.length; i++) {
+      const product = await this.getProductDetails(basicProducts[i]);
+      console.log("product: ", product);
+      products.push(product);
+    }
 
     console.log("products: ", products);
 
     return products;
   }
 
-  async getBasicProducts(item: string, page: Page): Promise<BasicProduct[]> {
+  async getBasicProducts(item: string): Promise<BasicProduct[]> {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
     const url = `https://www.amazon.com.br/s?k=${item}`;
     await page.goto(url);
 
@@ -78,6 +38,7 @@ export class AmazonScraping extends Scraping {
       for (let i = 0; i < msg.args().length; ++i)
         console.log(`${i}: ${msg.args()[i]}`);
     });
+
 
     const basicProducts: BasicProduct[] = await page.evaluate(() => {
       const productElements = Array.from(
@@ -120,24 +81,25 @@ export class AmazonScraping extends Scraping {
       return productsAux;
     });
 
+    browser.close();
 
     return basicProducts;
   }
 
-  async getProductsDetails(basicProducts: BasicProduct[], page: Page): Promise<Product[]> {
-    const products: Promise<Product>[] = basicProducts.map(async (basicProduct) => {
+  async getProductDetails(basicProduct: BasicProduct): Promise<Product> {
+    console.log("starting get product details, product: ", basicProduct);
+    const browser = await puppeteer.launch();
+      const page = await browser.newPage();
       await page.goto(basicProduct.link);
-      
-      const product: Product = await page.evaluate(() => {
+
+      const descriptionAndDatasheet: Info[] = await page.evaluate(() => {
         const descriptionChildrens = document.querySelector<HTMLBodyElement>("#productOverview_feature_div tbody")?.children;
 
-        if(!descriptionChildrens) {
-          return {
-            ...basicProduct
-          }
-        }
-        
         const description: Info[] = [];
+        
+        if(!descriptionChildrens) {
+          return description;
+        }
 
         for (let i = 0; i < descriptionChildrens.length; i++) {
           const text =(<HTMLElement>descriptionChildrens[i]).innerText.split('\t');
@@ -149,17 +111,11 @@ export class AmazonScraping extends Scraping {
           description.push(info);
         }
 
-        const product = {
-          ...basicProduct,
-          description
-        } as Product;
-
-        return product;
+        return description;
       });
 
-      return product;
-    });
+      browser.close();
 
-    return Promise.all(products);
-  }
+      return {...basicProduct, datasheet: descriptionAndDatasheet};
+    };
 }
